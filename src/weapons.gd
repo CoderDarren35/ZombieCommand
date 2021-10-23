2787553eecdf2c505c0df0511895fc06efea04ba
CoderDarren35 <darrenko23@gmail.com>
2021-10-22 17:56:41 -0700

    adding weapons

--- src/weapons.gd
+++ src/weapons.gd
@@ -1,11 +1,29 @@
-tool
-
 extends ItemList
 
-
+enum weapon{
+	UNARMED = -1,
+	RIFLE,
+	ROCKET
+}
+func fire():
+	var items := get_selected_items()
+	if not len(items):
+		return
+	elif len(items) > 1:
+		breakpoint
+	var item := items[0]
+	match item:
+		weapon.RIFLE:
+			pass
+		weapon.ROCKET:
+			pass
 
 func _ready():
 	add_item("rifle")
 	add_item("rocket")
 
+func _input(event):
+	if event.is_action_pressed("fire"):
+		fire()
+		
 
